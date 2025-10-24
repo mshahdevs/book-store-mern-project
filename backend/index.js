594c20a70200ec0380f8faa2449494460905b598
mshahdevs <mshahaurangzeb45@gmail.com>
2025-10-24 11:58:48 +0500

    set the path of env file

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "..", ".env") });
+
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
-
+const { connectDB } = require("./config/db");
+connectDB();
 app.get("/", (req, res) => {
   res.send("Server is up and running");
 });
